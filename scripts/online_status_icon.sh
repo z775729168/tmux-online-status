0a800c4a4a92a1878fbe0da865fbf7debd9122c2
z775729168 <z775729168@gmail.com>
2023-10-13 17:31:18 +0800

    Update online_status_icon.sh

--- scripts/online_status_icon.sh
+++ scripts/online_status_icon.sh
@@ -13,7 +13,7 @@ offline_icon_osx="⛔️ "
 offline_icon_cygwin="X"
 offline_icon="❌ "
 ping_timeout_default="3"
-route_to_ping_default="www.baidu.com"
+route_to_ping_default="cn.bing.com"
 
 source $CURRENT_DIR/shared.sh
 
